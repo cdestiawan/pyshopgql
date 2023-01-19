63f692d6a5c539b80948b9a76bd1bbdaa8d0ee02
Chandra Destiawan <cdestiawan@gmail.com>
2023-01-19 11:05:19 +0700

    add admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'description', 'slug', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ['name']}
+    inlines = (ProductImageInline,)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug')
+    search_fields = ('slug',)
+    prepopulated_fields = {'slug': ['title']}
--- catalog/models.py
+++ catalog/models.py
@@ -12,9 +12,16 @@ class Product(models.Model):
     category = models.ForeignKey(
         "category", on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     title = models.CharField(max_length=20)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
 
 
 class ProductImage(models.Model):
